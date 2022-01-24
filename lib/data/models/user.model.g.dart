3eb5a00c5fdb617b6ac6a1a1df729e65e4a5bc6a
brianmwangi <brnmwas@gmail.com>
2022-01-24 09:31:07 +0300

    Added missing files

---
+++ lib/data/models/user.model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      id: json['id'] as String,
+      email: json['email'] as String,
+      username: json['username'] as String,
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'username': instance.username,
+    };
